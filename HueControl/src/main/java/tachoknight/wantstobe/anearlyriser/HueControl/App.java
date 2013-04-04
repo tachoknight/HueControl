e8c738d4616d0fe9b31782d7b28df8e8e13a2871
rolson <rolson@lbpc.com>
2013-04-04 14:08:35 -0500

    First put of empty project

---
+++ HueControl/src/main/java/tachoknight/wantstobe/anearlyriser/HueControl/App.java
@@ -0,0 +1,13 @@
+package tachoknight.wantstobe.anearlyriser.HueControl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

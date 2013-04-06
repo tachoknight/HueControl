140a240c818f401afef7cd6da45c15afc1802d5b
Ron Olson <tachoknight@gmail.com>
2013-04-06 18:15:59 -0500

    Added LightFactory
    Added the beginnings of the LightFactory class and removed the sample code from App, which now uses the factory object to do everything.

--- HueControl/src/main/java/tachoknight/wantstobe/anearlyriser/App.java
+++ HueControl/src/main/java/tachoknight/wantstobe/anearlyriser/App.java
@@ -14,11 +14,13 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicHeader;
 import org.apache.http.protocol.HTTP;
-import org.codehaus.jackson.JsonFactory;
+
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ser.FilterProvider;
 import org.codehaus.jackson.map.ser.impl.SimpleBeanPropertyFilter;
 import org.codehaus.jackson.map.ser.impl.SimpleFilterProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import tachoknight.wantstobe.anearlyriser.model.LightsEntry;
 import tachoknight.wantstobe.anearlyriser.model.State;
@@ -30,66 +32,40 @@ import tachoknight.wantstobe.anearlyriser.model.SystemConfiguration;
  */
 public class App
 {
+	private static final Logger	logger	= LoggerFactory.getLogger(App.class);
+
 	public static void main(String[] args)
 	{
-		HttpClient client = new DefaultHttpClient();
+		/*
+		 * First we set up our lights factory
+		 */
+		LightsFactory lf = new LightsFactory("10.0.1.28", "newdeveloper");
 
-		HttpGet request = new HttpGet("http://" + "10.0.1.28" + "/api/" + "newdeveloper");
+		/* Get a dump of the configuration */
+		SystemConfiguration sc = lf.getSystemConfiguration();
+		logger.info(sc.getConfig().getName());
 
-		try
+		/* Get a map of the available lights */
+		Map<String, LightsEntry> lights = lf.getAllLights();
+		/* And loop through 'em for yucks */
+		for (Map.Entry<String, LightsEntry> entry : lights.entrySet())
 		{
-			HttpResponse response = client.execute(request);
-			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity()
-																					.getContent()));
-
-			ObjectMapper mapper = new ObjectMapper();
-			SystemConfiguration sc = mapper.readValue(rd, SystemConfiguration.class);
-
-			System.out.println(sc.getConfig().getName());
-			Map<String, LightsEntry> lights = sc.getLights();
-
-			LightsEntry light1 = lights.get("1");
-			System.out.println(light1.getName() + " - " + light1.getState().getBri());
-
-			//State light1State = light1.getState();
-			State light1State = new State();
-			light1State.setOn(true);
-			light1State.setBri(87);
-			light1State.setHue(15);
-			light1State.setSat(200);
-
-			HttpPut statePut = new HttpPut("http://" + "10.0.1.28"
-											+ "/api/"
-											+ "newdeveloper/lights/1/state");
-
-			ObjectMapper lightMapper = new ObjectMapper();
-
-			FilterProvider filters = new SimpleFilterProvider().addFilter(	"stateFilter",
-																			SimpleBeanPropertyFilter.serializeAllExcept("colormode","reachable"));
-
-			String light1JSON = lightMapper.writer(filters).writeValueAsString(light1State);
+			String key = entry.getKey();
+			LightsEntry value = entry.getValue();
 			
-			System.out.println("--> " + light1JSON);
-			StringEntity entity = new StringEntity(light1JSON);
-			entity.setContentType("application/json;charset=UTF-8");
-			entity.setContentEncoding(new BasicHeader(	HTTP.CONTENT_TYPE,
-														"application/json;charset=UTF-8"));
-			statePut.setEntity(entity);
-
-			HttpResponse light1Response = client.execute(statePut);
-			HttpEntity entity1 = light1Response.getEntity();
-			System.out.println(light1Response.getStatusLine());
-
+			logger.info("Light " + key + " - " + value.getName());
 		}
-		catch (IOException e)
+
+		/* Set a light */
+		State light1State = new State();
+		light1State.setOn(true);
+		light1State.setBri(87);
+		light1State.setHue(15);
+		light1State.setSat(200);
+
+		if (lf.setLightState(1, light1State) == false)
 		{
-			System.err.println("Drat, got " + e.getLocalizedMessage());
-			e.printStackTrace();
-		}
-		finally
-		{
-			request.releaseConnection();
-			client.getConnectionManager().closeExpiredConnections();
+			logger.error("Hmm...didn't set the light");
 		}
 
 		System.out.println("Hello World!");
---
+++ HueControl/src/main/java/tachoknight/wantstobe/anearlyriser/LightsFactory.java
@@ -0,0 +1,213 @@
+package tachoknight.wantstobe.anearlyriser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.StringEntity;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.ser.FilterProvider;
+import org.codehaus.jackson.map.ser.impl.SimpleBeanPropertyFilter;
+import org.codehaus.jackson.map.ser.impl.SimpleFilterProvider;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import tachoknight.wantstobe.anearlyriser.model.LightsEntry;
+import tachoknight.wantstobe.anearlyriser.model.State;
+import tachoknight.wantstobe.anearlyriser.model.SystemConfiguration;
+
+public class LightsFactory
+{
+	private static final Logger	logger	= LoggerFactory.getLogger(LightsFactory.class);
+
+	private String				ipAddress;
+	private String				userName;
+
+	public LightsFactory(String ipAddress, String userName)
+	{
+		this.ipAddress = ipAddress;
+		this.userName = userName;
+	}
+
+	private String getBaseUrl()
+	{
+		return "http://" + ipAddress + "/api/" + userName;
+	}
+
+	private boolean put(String url, String jsonToPut)
+	{
+		logger.debug("URL: " + url + "\nJSON:" + jsonToPut);
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPut putObject = new HttpPut(url);
+
+		try
+		{
+			StringEntity entity = new StringEntity(jsonToPut);
+			entity.setContentType("application/json;charset=UTF-8");
+			entity.setContentEncoding(new BasicHeader(	HTTP.CONTENT_TYPE,
+														"application/json;charset=UTF-8"));
+			putObject.setEntity(entity);
+
+			HttpResponse response = client.execute(putObject);
+
+			logger.debug("Response in put got: " + response.getStatusLine());
+
+			return true;
+		}
+		catch (IOException e)
+		{
+			logger.error("Hmm, got an error: " + e.getLocalizedMessage(), e);
+		}
+		finally
+		{
+			putObject.releaseConnection();
+			client.getConnectionManager().closeExpiredConnections();
+		}
+
+		logger.warn("Returning false to caller!");
+		return false;
+	}
+
+	public boolean setLightState(Integer numOfLight, State state)
+	{
+		String lightURL = getBaseUrl() + "/lights/" + numOfLight + "/state";
+
+		try
+		{
+			ObjectMapper stateMapper = new ObjectMapper();
+
+			/*
+			 * Certain properties are read-only and must be excluded from the
+			 * json we send to the server
+			 */
+			FilterProvider filters = new SimpleFilterProvider().addFilter(	"stateFilter",
+																			SimpleBeanPropertyFilter.serializeAllExcept("colormode",
+																														"reachable"));
+			String stateJSON = stateMapper.writer(filters)
+											.writeValueAsString(state);
+
+			return put(lightURL, stateJSON);
+		}
+		catch (JsonGenerationException e)
+		{
+			logger.error(	"Got a JsonGenerationException: " + e.getLocalizedMessage(),
+							e);
+		}
+		catch (JsonMappingException e)
+		{
+			logger.error(	"Got a JsonMappingException: " + e.getLocalizedMessage(),
+							e);
+		}
+		catch (IOException e)
+		{
+			logger.error("Got an IOException: " + e.getLocalizedMessage(), e);
+		}
+
+		return false;
+	}
+
+	private BufferedReader get(String url)
+	{
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(url);
+
+		try
+		{
+			HttpResponse response = client.execute(request);
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity()
+																					.getContent()));
+
+			return rd;
+		}
+		catch (IOException e)
+		{
+			logger.error("Hmm, got an error: " + e.getLocalizedMessage(), e);
+		}
+		finally
+		{
+			request.releaseConnection();
+			client.getConnectionManager().closeExpiredConnections();
+		}
+
+		logger.warn("Returning null to caller!");
+		return null;
+	}
+
+	/**
+	 * Gets a full dump of the Hue configuration, including all lights,
+	 * authorized devices, etc.
+	 * 
+	 * @return A {@link SystemConfiguration} object
+	 */
+	public SystemConfiguration getSystemConfiguration()
+	{
+		try
+		{
+			BufferedReader rd = get(getBaseUrl());
+
+			if (rd != null)
+			{
+				ObjectMapper mapper = new ObjectMapper();
+
+				return mapper.readValue(rd, SystemConfiguration.class);
+			}
+		}
+		catch (JsonParseException e)
+		{
+			logger.error(	"Got a JsonParserException: " + e.getLocalizedMessage(),
+							e);
+		}
+		catch (JsonMappingException e)
+		{
+			logger.error(	"Got a JsonMappingException: " + e.getLocalizedMessage(),
+							e);
+		}
+		catch (IOException e)
+		{
+			logger.error("Got an IOException: " + e.getLocalizedMessage(), e);
+		}
+
+		logger.warn("Returning null to caller!");
+		return null;
+	}
+
+	/**
+	 * Gets a map of light names (e.g. 1, 2, 3) to the light objects (of type
+	 * {@link LightsEntry}.
+	 * 
+	 * @return A map of Strings (Light Number) to {@link LightsEntry} or null
+	 */
+	public Map<String, LightsEntry> getAllLights()
+	{
+		return getSystemConfiguration().getLights();
+	}
+
+	/**
+	 * Gets a {@link LightsEntry} object that contains all the information about
+	 * that particular light
+	 * 
+	 * @param light
+	 *            number
+	 * @return A {@link LightsEntry} object or null
+	 */
+	public LightsEntry getLight(Integer numOfLight)
+	{
+		return getAllLights().get(numOfLight);
+	}
+}
